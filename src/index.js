7bf644d9bcc81b7cf8d25d6bacf4db19a787ba2f
Dmitry Tarasov <dmitry.tarasov@live.com>
2019-07-05 21:46:29 +0500

    rc

--- src/index.js
+++ src/index.js
@@ -4,4 +4,34 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var res = 0;
+
+    for (var i = 0; i < preferences.length; i++) {
+
+        var x = preferences[i];
+
+        x--;
+
+        if (x >= preferences.length) {
+            continue;
+        }
+
+        var y = preferences[x];
+
+        y--;
+
+        if (y <= x || y < i) {
+            continue;
+        }
+
+        if (y >= preferences.length) {
+            continue;
+        }
+
+        if (preferences[y] === i + 1) {
+            res++;
+        }
+    }
+
+    return res;
 };
